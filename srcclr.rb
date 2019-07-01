fb97e6850eeb5d6dab787ac8a833a1207a4ffac6
Shubhanshu Verma <shubhanshu.verma@harness.io>
2019-07-01 16:32:58 +0530

    Create srcclr.rb
    sample brew

---
+++ srcclr.rb
@@ -0,0 +1,13 @@
+require "formula"
+
+class Srcclr < Formula
+  homepage "https://srcclr.com"
+  url "https://download.sourceclear.com/console/srcclr-console-0.7.0.zip"
+  sha256 "6fdae5ff1adf92c5f99821cf1aceae8144686bf71350ccf0bbabb526da14de10"
+
+  def install
+      inreplace "brew/srcclr", "##PREFIX##", "#{prefix}"
+      prefix.install "srcclr-console.jar"
+      bin.install "brew/srcclr"
+  end
+end
